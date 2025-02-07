a81111c5865e9bef880ce3351980fdf32f7b4413
Arun2880 <160330026+Arun2880@users.noreply.github.com>
2025-02-07 11:02:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const shopRefferal = require ('./routes/shop/refferal-routes');
 const addInfluencers = require ('./routes/influencer/influencer-route')
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/erika', {
   useNewUrlParser: true,
